f16a8b6b74ebd87e769475b023a3c3a22a7b6c60
Jean Emmanuel C. Trinidad <jmtrinidad22@gmail.com>
2025-05-17 18:17:00 +0800

    last na orinmuse

--- src/app/student/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import AddStudent from "@/app/components/student/student";
-export default function Home() {
-    return (
-        <AddStudent />
-    );
-}
